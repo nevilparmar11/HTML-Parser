786c76100d8e85f593c85dc99d4d57f1d64158c2
nevilparmar11 <nevilparmar24@gmail.com>
2020-06-09 19:54:06 +0530

    heroku prepare

--- controller/index.js
+++ controller/index.js
@@ -40,7 +40,7 @@ app.use('/', function (req, res) {
 });
 
 const server = http.createServer(app);
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port);
 
 console.debug('Server listening on port ' + port);
